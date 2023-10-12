04b4c66f958008a12bcfb5bf2cbd07df15d462ed
Mathias Millet <mathias@mmill.eu>
2023-10-12 07:02:07 +0200

    first commit

---
+++ gen_playlist.py
@@ -0,0 +1,80 @@
+import dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import typer
+import logging
+from enum import Enum
+from typing import Literal
+
+
+dotenv.load_dotenv()
+scope = "playlist-modify-public,playlist-modify-private"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+
+def make_playlist(title):
+    playlist = sp.user_playlist_create(
+        user=sp.current_user()["id"],
+        name=title
+    )
+    return playlist
+
+
+def get_tracks_from_plages_musicales(input: str):
+    for line in input.splitlines():
+        artist, title = line.split("\t")
+        if title.startswith("single  :"):
+            title = title.split("single  :")[1].strip()
+            tracks = title.split(" + ")
+            for track in tracks:
+                results = sp.search(q=f"artist:{artist} track:{track}", type="track")["tracks"]
+                if len(results["items"]) == 0:
+                    pass
+                else:
+                    yield results["items"][0]
+        else:
+            results = sp.search(q=f"artist:{artist} album:{title}", type="album")["albums"]
+            if len(results["items"]) == 0:
+                pass
+            tracks = sp.album_tracks(results["items"][0]["uri"])
+            for track in tracks["items"]:
+                yield track
+
+
+def get_tracks_from_stoned_circus(input: str):
+    for line in input.splitlines():
+        artist = ""
+        track = ""
+        add_to_track = False
+        for word in line.split():
+            if add_to_track:
+                track += " " + word
+            else:
+                if word != word.upper():
+                    add_to_track = True
+                    track += " " + word
+                else:
+                    artist += " " + word
+            results = sp.search(q=f"artist:{artist} track:{track}", type="track")["tracks"]
+            if len(results["items"]) == 0:
+                pass
+            else:
+                yield results["items"][0]
+
+class Source(Enum):
+    stoned_circus = (get_tracks_from_stoned_circus,)
+    plages_musicales = (get_tracks_from_plages_musicales,)
+
+    def __init__(self, val):
+        self.val = val
+
+    @property
+    def value(self):
+        return self.name
+
+def main(source: Source, value: str):
+    for val in source.val(value):
+        print(val)
+
+typer.run(main)
