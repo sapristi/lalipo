530da131ecb8fcb78d7f62a9463bfaca3b2be8f8
Mathias Millet <mathias@mmill.eu>
2023-10-19 22:24:50 +0200

    adapt for systemd

--- lalipo/settings.py
+++ lalipo/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATE_DIR = Path(os.environ.get("STATE_DIR", BASE_DIR / "runtime"))
+STATE_DIR = Path(os.environ.get("STATE_DIRECTORY", BASE_DIR / "runtime"))
 
 
 # Quick-start development settings - unsuitable for production
--- lalipo/wsgi.py
+++ lalipo/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lalipo.settings')
 
+if "CREDENTIALS_DIRECTORY" in os.environ:
+    creds_dir = Path(os.environ["CREDENTIALS_DIRECTORY"])
+    print(f"Loading credentials from {creds_dir}")
+    for creds_file in creds_dir.iterdir():
+        os.environ[creds_file.name] = creds_file.read_text()
 application = get_wsgi_application()
